581cbda21f137c97a5a79de922be8455ea74aa5b
Shawn Moss <shawnrmoss@gmail.com>
2021-04-29 01:06:32 -0600

    CLeanup

--- plutus-base/build.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]; then
-    TAG="latest"
-else
-    TAG=$1
-fi
-docker login
-docker build -t farmrone/plutus-base:$TAG .
-docker push farmrone/plutus-base:$TAG
--- plutus-playground/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Start plutus playground client
-echo "Setting up plutus playground server..."
-cd /opt/plutus/plutus-playground-client
-sed -i -e 's/localhost:8080/server:8080/g' default.nix 
-sed -i -e 's/localhost:8080/server:8080/g' webpack.config.js
-sed -i -e 's/--progress/--progress --host=0.0.0.0/g' package.json
